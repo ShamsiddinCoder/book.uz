4084ca90f72fa0006dca9a50d6272bbebf49053b
ShamsiddinCoder <frenkk89@mail.ru>
2024-11-30 13:07:42 +0500

    feat: added husky hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { Button } from "./components/ui/button"
+import { Button } from "./components/ui/button";
 
 function App() {
-
-  return (
-    <>
-      <div>
-        <h3> Hello </h3>
-        <Button>New Button</Button>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div>
+				<h3> Hello </h3>
+				<Button>New Button</Button>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
